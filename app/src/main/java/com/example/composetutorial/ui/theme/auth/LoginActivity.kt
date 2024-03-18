bd7627f271617fd91e9478112e3bc5be9845d157
Yug Mewada <yugm@hyperlinkinfosystem.net.in>
2024-03-18 19:10:04 +0530

    [+]Added Login screen design

--- app/src/main/java/com/example/composetutorial/ui/theme/auth/LoginActivity.kt
+++ app/src/main/java/com/example/composetutorial/ui/theme/auth/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.composetutorial.ui.theme.auth
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -8,35 +9,42 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composetutorial.R
 import com.example.composetutorial.custom.BlueButton
 import com.example.composetutorial.custom.TextFields.CommonTextField
+import com.example.composetutorial.ui.theme.home.screens.HomeActivity
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class LoginActivity : ComponentActivity() {
@@ -63,13 +71,12 @@ class LoginActivity : ComponentActivity() {
     @Composable
     @Preview(name = "Login Background", showSystemUi = true, device = Devices.DEFAULT)
     private fun SetBackground() {
-        Box(
+        Surface(
             modifier = Modifier
                 .fillMaxSize()
                 .scrollable(
                     orientation = Orientation.Vertical, state = rememberScrollState()
                 )
-
         ) {
             Image(
                 contentScale = ContentScale.FillBounds,
@@ -77,17 +84,14 @@ class LoginActivity : ComponentActivity() {
                 contentDescription = "Login Screen Background",
                 modifier = Modifier.fillMaxSize()
             )
-
-
             Column(
-                verticalArrangement = Arrangement.Center,
                 modifier = Modifier
-                    .fillMaxSize()
+                    .fillMaxWidth()
                     .padding(start = 25.dp, end = 25.dp)
             ) {
                 Text(
                     text = "Login",
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier,
                     fontFamily = FontFamily(Font(R.font.sf_bold)),
                     fontSize = 24.sp,
                 )
@@ -133,11 +137,13 @@ class LoginActivity : ComponentActivity() {
                 ) {
 
                     if (isNameEmpty) {
-                        Toast.makeText(this@LoginActivity, "Please enter name", Toast.LENGTH_SHORT)
-                            .show()
+                        Toast.makeText(
+                            this@LoginActivity, "Please enter name", Toast.LENGTH_SHORT
+                        ).show()
                     } else if (isPasswordEmpty) {
-                        Toast.makeText(this@LoginActivity, "Please password", Toast.LENGTH_SHORT)
-                            .show()
+                        Toast.makeText(
+                            this@LoginActivity, "Please password", Toast.LENGTH_SHORT
+                        ).show()
                     } else {
                         Toast.makeText(this@LoginActivity, "Login Success", Toast.LENGTH_SHORT)
                             .show()
@@ -157,11 +163,22 @@ class LoginActivity : ComponentActivity() {
                 )
 
 
+            }
+
+
+            Column(
+                verticalArrangement = Arrangement.Bottom,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+
+            ) {
+
                 Row(
+                    horizontalArrangement = Arrangement.Center,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(top = 25.dp),
-                    horizontalArrangement = Arrangement.Center
+                        .padding(top = 25.dp)
                 ) {
                     Image(
                         painter = painterResource(id = R.drawable.ic_apple),
@@ -179,7 +196,66 @@ class LoginActivity : ComponentActivity() {
                         contentDescription = "Fb Login"
                     )
                 }
+
+                val signup = "Sign up"
+                val annotatedStringSignUp = createSpannableText(
+                    mainText = "Don’t have an account? ",
+                    text = signup,
+                    color = colorResource(id = R.color.colorBlue2),
+                    fontSize = 16.sp,
+                    fontFamily = FontFamily(Font(R.font.sf_bold)),
+                    underlineText = "Yug"
+                )
+
+                ClickableText(
+                    modifier = Modifier.padding(top = 25.dp, bottom = 20.dp),
+                    text = annotatedStringSignUp,
+                    style = TextStyle(fontFamily = FontFamily(Font(R.font.sf_regular))),
+                    onClick = { offset ->
+                        annotatedStringSignUp.getStringAnnotations("ClickableSpan", offset, offset)
+                            .firstOrNull()?.let { span ->
+                                startActivity(Intent(this@LoginActivity, HomeActivity::class.java))
+                                println(this.hashCode())
+                            }
+                    })
+
             }
         }
+    }
+
+    @Composable
+    private fun createSpannableText(
+        mainText:String,
+        text: String,
+        color: Color,
+        fontSize: TextUnit,
+        fontFamily: FontFamily,
+        underlineText: String? = null
+    ): AnnotatedString {
+        val words = text.split(" ")
+
+        return AnnotatedString.Builder().apply {
+            append(mainText)
+            words.forEachIndexed { index, word ->
+                val start = length
+                val end = start + word.length
+
+                val style = if (word == underlineText) {
+                    SpanStyle(textDecoration = TextDecoration.Underline)
+                } else {
+                    SpanStyle(color = color, fontSize = fontSize, fontFamily = fontFamily)
+                }
+
+                addStyle(style, start, end)
+
+                if (word == underlineText) {
+                    addStringAnnotation("ClickableSpan", "ClickableSpan", start, end)
+                }
+
+                append(word)
+
+                if (index < words.size - 1) append(" ")
+            }
+        }.toAnnotatedString()
     }
 }
